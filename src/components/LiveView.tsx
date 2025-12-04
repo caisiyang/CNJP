a65849d1423f7b8f28cd95961872041abcd954d7
caisiyang <caisiyang@gmail.com>
2025-12-04 20:35:48 +0900

    恢复东京湾直播

--- src/components/LiveView.tsx
+++ src/components/LiveView.tsx
@@ -116,7 +116,7 @@ export default function LiveView() {
                         </span>
                     </div>
 
-                    <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-2">
+                    <div className="grid grid-cols-3 gap-2">
                         {data.streams.map(stream => (
                             <button
                                 key={stream.id}
